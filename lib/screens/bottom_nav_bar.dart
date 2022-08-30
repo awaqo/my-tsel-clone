4b5a2ddae20d5e8f6c0a6d00a676ccfcb57dcb83
awaqo <aqiljawadalfurqon@gmail.com>
2022-08-30 14:49:02 +0700

    add assets, text & color style, bottom nav bar

---
+++ lib/screens/bottom_nav_bar.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:my_tsel_clone/themes.dart';
+
+class BottomNavBar extends StatefulWidget {
+  const BottomNavBar({super.key});
+
+  @override
+  State<BottomNavBar> createState() => _BottomNavBarState();
+}
+
+class _BottomNavBarState extends State<BottomNavBar> {
+  int _selectedIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    Widget customNavBar() {
+      return BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: (i) => setState(() => _selectedIndex = i),
+        type: BottomNavigationBarType.fixed,
+        selectedLabelStyle: bold12.copyWith(color: grey3Color),
+        unselectedLabelStyle: bold12.copyWith(color: grey3Color),
+        items: [
+          BottomNavigationBarItem(
+            icon: Container(
+                margin: EdgeInsets.symmetric(vertical: 5),
+                child: Image.asset(
+                  'assets/icons/icon-home.png',
+                  height: 20,
+                  color: _selectedIndex == 0 ? red2Color : grey3Color,
+                )),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Container(
+                margin: EdgeInsets.symmetric(vertical: 5),
+                child: Image.asset(
+                  'assets/icons/icon-trofi.png',
+                  height: 20,
+                  color: _selectedIndex == 1 ? red2Color : grey3Color,
+                )),
+            label: 'POIN',
+          ),
+          BottomNavigationBarItem(
+            icon: Container(
+                margin: EdgeInsets.symmetric(vertical: 5),
+                child: Image.asset(
+                  'assets/icons/icon-cart.png',
+                  height: 20,
+                  color: _selectedIndex == 2 ? red2Color : grey3Color,
+                )),
+            label: 'Shop',
+          ),
+          BottomNavigationBarItem(
+            icon: Container(
+                margin: EdgeInsets.symmetric(vertical: 5),
+                child: Image.asset(
+                  'assets/icons/icon-rocket.png',
+                  height: 20,
+                  color: _selectedIndex == 3 ? red2Color : grey3Color,
+                )),
+            label: 'Explore',
+          ),
+          BottomNavigationBarItem(
+            icon: Container(
+                margin: EdgeInsets.symmetric(vertical: 5),
+                child: Image.asset(
+                  'assets/icons/icon-user.png',
+                  height: 20,
+                  color: _selectedIndex == 4 ? red2Color : grey3Color,
+                )),
+            label: 'Menu',
+          ),
+        ],
+      );
+    }
+
+    return Scaffold(
+      bottomNavigationBar: customNavBar(),
+    );
+  }
+}
---
+++ lib/themes.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+// COLOR STYLE
+Color pinkColor = Color(0xFFFEE5E7);
+Color red1Color = Color(0xFFF00027);
+Color red2Color = Color(0xFFDF181D);
+Color red3Color = Color(0xFFBA0024);
+Color blue1Color = Color(0xFF124888);
+Color blue2Color = Color(0xFF1D13E5);
+Color blue3Color = Color(0xFF011C40);
+Color grey1Color = Color(0xFFF5F4F2);
+Color grey2Color = Color(0xFFEDECF1);
+Color grey3Color = Color(0xFFB4B8BB);
+Color grey4Color = Color(0xFF535760);
+Color yellowColor = Color(0xFFEDD501);
+Color orange1Color = Color(0xFFFAA424);
+Color orange2Color = Color(0xFFB54618);
+
+// TEXT STYLE
+FontWeight reguler = FontWeight.w400;
+FontWeight medium = FontWeight.w500;
+FontWeight semiBold = FontWeight.w600;
+FontWeight bold = FontWeight.w700;
+FontWeight extraBold = FontWeight.w800;
+
+TextStyle reguler10 = GoogleFonts.poppins(fontSize: 10, fontWeight: reguler);
+TextStyle reguler12 = GoogleFonts.poppins(fontSize: 12, fontWeight: reguler);
+TextStyle reguler13 = GoogleFonts.poppins(fontSize: 13, fontWeight: reguler);
+TextStyle reguler14 = GoogleFonts.poppins(fontSize: 14, fontWeight: reguler);
+
+TextStyle medium9 = GoogleFonts.poppins(fontSize: 9, fontWeight: medium);
+TextStyle medium10 = GoogleFonts.poppins(fontSize: 10, fontWeight: medium);
+TextStyle medium12 = GoogleFonts.poppins(fontSize: 12, fontWeight: medium);
+TextStyle medium13 = GoogleFonts.poppins(fontSize: 13, fontWeight: medium);
+TextStyle medium15 = GoogleFonts.poppins(fontSize: 15, fontWeight: medium);
+
+TextStyle semibold13 = GoogleFonts.poppins(fontSize: 13, fontWeight: semiBold);
+
+TextStyle bold12 = GoogleFonts.poppins(fontSize: 12, fontWeight: bold);
+TextStyle bold13 = GoogleFonts.poppins(fontSize: 13, fontWeight: bold);
+TextStyle bold14 = GoogleFonts.poppins(fontSize: 14, fontWeight: bold);
+TextStyle bold15 = GoogleFonts.poppins(fontSize: 15, fontWeight: bold);
+
+TextStyle extraBold18 = GoogleFonts.nunito(fontSize: 18, fontWeight: extraBold);
+TextStyle extraBold26 = GoogleFonts.poppins(fontSize: 26, fontWeight: extraBold);
